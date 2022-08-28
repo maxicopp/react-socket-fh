96f8504fa35f1c98f4627fe9cc2ea6db76132ca0
Maximiliano Cóppola <maxicopp@outlook.com>
2022-08-28 03:27:10 -0300

    Mi primer Socket Server

---
+++ 01-socket-server-basic/index.js
@@ -0,0 +1,13 @@
+
+
+const app = require('express')()
+
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server)
+
+io.on('connection', () => { /* … */ })
+
+server.listen(8080, () => {
+    console.log('Server corriendo en puerto :8080')
+});
