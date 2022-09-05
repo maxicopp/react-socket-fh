e0e9a6527ca97be941c1d91c542f9000f5f2658d
Maximiliano Cóppola <maxicopp@outlook.com>
2022-09-05 00:43:15 -0300

    habilitar CORS

--- 01-socket-server-basic/models/server.js
+++ 01-socket-server-basic/models/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 
 const Sockets = require('./sockets');
 
@@ -19,6 +20,8 @@ class Server {
 
     middlewares() {
         this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+        this.app.use(cors());
     }
 
     configurarSockets() {
