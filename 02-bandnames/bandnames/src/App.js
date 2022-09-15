a04a82b7bd3ed241224058c236815fb324efe480
Maximiliano Cóppola <maxicopp@outlook.com>
2022-09-15 12:36:13 -0300

    useSocket - CustomHook

--- 02-bandnames/bandnames/src/App.js
+++ 02-bandnames/bandnames/src/App.js
@@ -1,36 +1,14 @@
 import { useEffect, useState } from 'react';
-import io from 'socket.io-client';
+
 
 import { BandAdd } from './components/BandAdd';
 import { BandList } from './components/BandList';
-
-const connectSocketServer = () => {
-  const socket = io.connect('http://localhost:8080', {
-    transports: ['websocket']
-  });
-  return socket;
-}
+import { useSocket } from './hooks/useSocket';
 
 function App() {
-  const [socket] = useState(connectSocketServer());
-  const [online, setOnline] = useState(false);
   const [bands, setBands] = useState([]);
 
-  useEffect(() => {
-    setOnline(socket.connected);
-  }, [socket]);
-
-  useEffect(() => {
-    socket.on('connect', () => {
-      setOnline(true);
-    });
-  }, [socket]);
-
-  useEffect(() => {
-    socket.on('disconnect', () => {
-      setOnline(false);
-    });
-  }, [socket]);
+  const { socket, online } = useSocket('http://localhost:8080');
 
   useEffect(() => {
     socket.on('current-bands', (bands) => {
