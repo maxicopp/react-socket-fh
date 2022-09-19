49583e2e6cc0de23230ac08506b4c18c3d1bf0d0
Maximiliano Cóppola <maxicopp@outlook.com>
2022-09-19 19:18:20 -0300

    Pantalla de escritorio

--- 03-ticket-app/ticket-app/src/pages/Escritorio.js
+++ 03-ticket-app/ticket-app/src/pages/Escritorio.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import { Button, Col, Divider, Row, Typography } from 'antd';
+import { CloseCircleOutlined, RightOutlined } from '@ant-design/icons';
+
+const { Title, Text } = Typography;
 
 export const Escritorio = () => {
+    const salir = () => {
+        console.log('salir');
+    }
+
+    const siguienteTicket = () => {
+        console.log('siguienteTicket');
+    }
     return (
-        <div>Escritorio</div>
+        <>
+            <Row>
+                <Col span={20}>
+                    <Title level={2}>Maximiliano</Title>
+                    <Text>Usted está trabajando en el escritorio: </Text>
+                    <Text type="success">5</Text>
+                </Col>
+
+                <Col span={4} align="right">
+                    <Button
+                        shape="round"
+                        type="danger"
+                        onClick={salir}
+                    >
+                        <CloseCircleOutlined />
+                        Salir
+                    </Button>
+                </Col>
+            </Row>
+
+            <Divider />
+
+            <Row>
+                <Col>
+                    <Text>Está atendiendo el ticket: </Text>
+                    <Text
+                        style={{ fontSize: 30 }}
+                        type="danger"
+                    >
+                        55
+                    </Text>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col offset={18} span={6} align="right">
+                    <Button
+                        onClick={siguienteTicket}
+                        shape="round"
+                        type="primary"
+                    >
+                        <RightOutlined />
+                        Siguiente
+                    </Button>
+                </Col>
+            </Row>
+        </>
     )
 }
--- 03-ticket-app/ticket-app/src/pages/Ingresar.js
+++ 03-ticket-app/ticket-app/src/pages/Ingresar.js
@@ -1,72 +1,84 @@
 import React from 'react';
-import { Button, Form, Input, InputNumber } from 'antd';
+import { Button, Divider, Form, Input, InputNumber, Typography } from 'antd';
 import { SaveOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router-dom';
+
+const { Title, Text } = Typography;
 
 export const Ingresar = () => {
+
+    const history = useHistory();
+
     const onFinish = (values) => {
         console.log('Success:', values);
+        history.push('/escritorio');
     };
 
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
     };
     return (
-        <Form
-            name="basic"
-            labelCol={{
-                span: 8,
-            }}
-            wrapperCol={{
-                span: 14,
-            }}
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Form.Item
-                label="Nombre del agente"
-                name="agente"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Por favor, ingrese su nombre',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-
-            <Form.Item
-                label="Escritorio"
-                name="escritorio"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Ingrese el número de escritorio',
-                    },
-                ]}
-            >
-                <InputNumber min={1} max={99} />
-            </Form.Item>
-
-            <Form.Item
+        <>
+            <Title level={2}>Ingresar</Title>
+            <Text>Ingrese su nombre y número de escritorio</Text>
+            <Divider />
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
                 wrapperCol={{
-                    offset: 8,
                     span: 14,
                 }}
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
             >
-                <Button
-                    type="primary"
-                    htmlType="submit"
-                    shape="round"
+                <Form.Item
+                    label="Nombre del agente"
+                    name="agente"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Por favor, ingrese su nombre',
+                        },
+                    ]}
                 >
-                    <SaveOutlined />
-                    Ingresar
-                </Button>
-            </Form.Item>
-        </Form>
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Escritorio"
+                    name="escritorio"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Ingrese el número de escritorio',
+                        },
+                    ]}
+                >
+                    <InputNumber min={1} max={99} />
+                </Form.Item>
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 14,
+                    }}
+                >
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        shape="round"
+                    >
+                        <SaveOutlined />
+                        Ingresar
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
     )
 }
