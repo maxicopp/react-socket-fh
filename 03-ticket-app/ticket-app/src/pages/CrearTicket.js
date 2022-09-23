a5d7ee613d2a4aff8f5ce01ab979c0cfa14b6e8c
Maximiliano Cóppola <maxicopp@outlook.com>
2022-09-23 13:11:09 -0300

    Obtener un nuevo ticket

--- 03-ticket-app/ticket-app/src/pages/CrearTicket.js
+++ 03-ticket-app/ticket-app/src/pages/CrearTicket.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Col, Row, Typography } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
 import { useHideMenu } from '../hooks/useHideMenu';
+import { SocketContext } from '../context/SocketContext';
 
 const { Title, Text } = Typography;
 
@@ -9,8 +10,13 @@ export const CrearTicket = () => {
 
     useHideMenu(true);
 
+    const { socket } = useContext(SocketContext);
+    const [ticket, setTicket] = useState(null);
+
     const nuevoTicket = () => {
-        console.log('nuevoTicket');
+        socket.emit('solicitar-ticket', null, (ticket) => {
+            setTicket(ticket);
+        });
     }
 
     return (
@@ -33,17 +39,21 @@ export const CrearTicket = () => {
                 </Col>
             </Row>
 
-            <Row style={{ marginTop: 100 }}>
-                <Col span={14} offset={6} align="center">
-                    <Text level={2}>
-                        Su número
-                    </Text>
-                    <br />
-                    <Text type="success" style={{ fontSize: 55 }}>
-                        55
-                    </Text>
-                </Col>
-            </Row>
+            {
+                ticket && (
+                    <Row style={{ marginTop: 100 }}>
+                        <Col span={14} offset={6} align="center">
+                            <Text level={2}>
+                                Su número
+                            </Text>
+                            <br />
+                            <Text type="success" style={{ fontSize: 55 }}>
+                                {ticket.numero}
+                            </Text>
+                        </Col>
+                    </Row>
+                )
+            }
         </>
     )
 }
--- 03-ticket-app/ticket-server/models/sockets.js
+++ 03-ticket-app/ticket-server/models/sockets.js
@@ -1,25 +1,25 @@
-
-
+const TicketList = require('./ticket-list');
 class Sockets {
 
-    constructor( io ) {
+    constructor(io) {
 
         this.io = io;
+
+        // Crear la instancia de nuestro ticketlist
+        this.ticketList = new TicketList();
 
         this.socketEvents();
     }
 
     socketEvents() {
         // On connection
-        this.io.on('connection', ( socket ) => {
+        this.io.on('connection', (socket) => {
 
             console.log('cliente conectado');
 
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-
-                this.io.emit('mensaje-from-server', data );
+            socket.on('solicitar-ticket', (data, callback) => {
+                const nuevoTicket = this.ticketList.crearTicket();
+                callback(nuevoTicket);
             });
 
 
