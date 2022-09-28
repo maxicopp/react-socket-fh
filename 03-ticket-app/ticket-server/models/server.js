047301dc6f7812fc8276b2e2471ffa2bbc7e494e
Maximiliano Cóppola <maxicopp@outlook.com>
2022-09-28 19:15:11 -0300

    Servicio REST para obtener los tickets actuales

--- 03-ticket-app/ticket-server/models/server.js
+++ 03-ticket-app/ticket-server/models/server.js
@@ -1,39 +1,44 @@
 // Servidor de Express
-const express  = require('express');
-const http     = require('http');
+const express = require('express');
+const http = require('http');
 const socketio = require('socket.io');
-const path     = require('path');
-const cors     = require('cors');
+const path = require('path');
+const cors = require('cors');
 
-const Sockets  = require('./sockets');
+const Sockets = require('./sockets');
 
 class Server {
 
     constructor() {
 
-        this.app  = express();
+        this.app = express();
         this.port = process.env.PORT;
 
         // Http server
-        this.server = http.createServer( this.app );
+        this.server = http.createServer(this.app);
 
         // Configuraciones de sockets
-        this.io = socketio( this.server, { /* configuraciones */ } );
+        this.io = socketio(this.server, { /* configuraciones */ });
+
+        // Inicializar sockets
+        this.sockets = new Sockets(this.io);
     }
 
     middlewares() {
         // Desplegar el directorio público
-        this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
+        this.app.use(express.static(path.resolve(__dirname, '../public')));
 
         // CORS
-        this.app.use( cors() );
+        this.app.use(cors());
 
-    }
+        // get de los últimos 13
+        this.app.get('/ultimos', (req, res) => {
+            res.json({
+                ok: true,
+                ultimos: this.sockets.ticketList.ultimos13
+            });
+        });
 
-    // Esta configuración se puede tener aquí o como propieda de clase
-    // depende mucho de lo que necesites
-    configurarSockets() {
-        new Sockets( this.io );
     }
 
     execute() {
@@ -41,12 +46,9 @@ class Server {
         // Inicializar Middlewares
         this.middlewares();
 
-        // Inicializar sockets
-        this.configurarSockets();
-
         // Inicializar Server
-        this.server.listen( this.port, () => {
-            console.log('Server corriendo en puerto:', this.port );
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo en puerto:', this.port);
         });
     }
 
