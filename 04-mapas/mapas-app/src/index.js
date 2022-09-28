57dae73690f440791f76a93755839982b6260318
Maximiliano Cóppola <maxicopp@outlook.com>
2022-09-28 19:45:53 -0300

    MapasApp

---
+++ 04-mapas/mapas-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { MapasApp } from './MapasApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <MapasApp />
+);
