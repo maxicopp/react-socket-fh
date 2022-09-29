9c61ad75014ae5d629a45e266522ccb9d3d92865
Maximiliano Cóppola <maxicopp@outlook.com>
2022-09-29 12:37:03 -0300

    Mantener la referencia de las coordenadas

--- 04-mapas/mapas-app/src/pages/MapaPage.js
+++ 04-mapas/mapas-app/src/pages/MapaPage.js
@@ -3,14 +3,15 @@ import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF4aWNvcHAiLCJhIjoiY2w4bTduZmx6MDY4bjN1cGoxMDlweGdneCJ9.6CZEiE5QYIuDjhuEmMoxcA';
 const puntoInicial = {
-    lng: 5,
-    lat: 34,
-    zoom: 10
+    lng: -58.3690,
+    lat: -34.6680,
+    zoom: 17
 };
 
 export const MapaPage = () => {
     const mapaDiv = useRef();
-    const [, setMapa] = useState();
+    const [mapa, setMapa] = useState();
+    const [coords, setCoords] = useState(puntoInicial);
 
     useEffect(() => {
         const map = new mapboxgl.Map({
@@ -23,8 +24,24 @@ export const MapaPage = () => {
         setMapa(map);
     }, []);
 
+    // Cuando se mueve el mapa
+    useEffect(() => {
+        mapa?.on('move', () => {
+            const { lng, lat } = mapa.getCenter();
+            setCoords({
+                lng: lng.toFixed(4),
+                lat: lat.toFixed(4),
+                zoom: mapa.getZoom().toFixed(2)
+            });
+        });
+    }, [mapa]);
+
+
     return (
         <>
+            <div className="info">
+                Lng: {coords.lng} | lat: {coords.lat} | zoom: {coords.zoom}
+            </div>
             <div ref={mapaDiv} className="mapContainer" />
         </>
     )
