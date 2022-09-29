5cb62247a5b6abb553d5dd84d2ac4aebcc15a1eb
Maximiliano Cóppola <maxicopp@outlook.com>
2022-09-29 14:34:33 -0300

    Agregar marcadores

--- 04-mapas/mapas-app/src/hooks/useMapbox.js
+++ 04-mapas/mapas-app/src/hooks/useMapbox.js
@@ -1,11 +1,16 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import { v4 } from 'uuid';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF4aWNvcHAiLCJhIjoiY2w4bTduZmx6MDY4bjN1cGoxMDlweGdneCJ9.6CZEiE5QYIuDjhuEmMoxcA';
 
 export const useMapbox = (puntoInicial) => {
     const mapaDiv = useRef();
+    // Mapa y coords
     const mapa = useRef();
+    // Referencia de los marcadores
+    const marcadores = useRef({});
+
     const [coords, setCoords] = useState(puntoInicial);
 
     const setRef = useCallback((node) => { mapaDiv.current = node }, []);
@@ -33,8 +38,22 @@ export const useMapbox = (puntoInicial) => {
         });
     }, []);
 
+    // Agregar marcadores cuando hago click
+    useEffect(() => {
+        mapa.current?.on('click', (ev) => {
+            const { lng, lat } = ev.lngLat;
+            const marker = new mapboxgl.Marker();
+            marker.id = v4(); // TODO: Si el marcador ya tiene id
+
+            marker.setLngLat([lng, lat]).addTo(mapa.current).setDraggable(true);
+
+            marcadores.current[marker.id] = marker;
+        });
+    }, []);
+
     return {
         coords,
+        marcadores,
         setRef
     }
 }
