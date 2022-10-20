d66c8457ade39e3040adf9bce53c58360fc68747
Maximiliano Cóppola <maxicopp@outlook.com>
2022-10-20 12:23:47 -0300

    Definición de rutas de autenticación

--- 05-chat/chat-backend/models/server.js
+++ 05-chat/chat-backend/models/server.js
@@ -1,38 +1,43 @@
 // Servidor de Express
-const express  = require('express');
-const http     = require('http');
+const express = require('express');
+const http = require('http');
 const socketio = require('socket.io');
-const path     = require('path');
+const path = require('path');
 
-const Sockets  = require('./sockets');
+const Sockets = require('./sockets');
 const { dbConnection } = require('../database/config');
 
 class Server {
 
     constructor() {
 
-        this.app  = express();
+        this.app = express();
         this.port = process.env.PORT;
 
         // Conectar a DB
         dbConnection();
 
         // Http server
-        this.server = http.createServer( this.app );
+        this.server = http.createServer(this.app);
 
         // Configuraciones de sockets
-        this.io = socketio( this.server, { /* configuraciones */ } );
+        this.io = socketio(this.server, { /* configuraciones */ });
     }
 
     middlewares() {
         // Desplegar el directorio público
-        this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
+        this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+        // TODO: CORS
+
+        // API ENDPoints
+        this.app.use('/api/login', require('../router/auth'));
     }
 
     // Esta configuración se puede tener aquí o como propieda de clase
     // depende mucho de lo que necesites
     configurarSockets() {
-        new Sockets( this.io );
+        new Sockets(this.io);
     }
 
     execute() {
@@ -44,8 +49,8 @@ class Server {
         this.configurarSockets();
 
         // Inicializar Server
-        this.server.listen( this.port, () => {
-            console.log('Server corriendo en puerto:', this.port );
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo en puerto:', this.port);
         });
     }
 
