cda2ef3af47bfe3f467c16eaf4e6ace3382d191b
Maximiliano Cóppola <maxicopp@outlook.com>
2022-10-21 22:45:09 -0300

    Crear usuario en base de datos

--- 05-chat/chat-backend/controllers/auth.js
+++ 05-chat/chat-backend/controllers/auth.js
@@ -1,16 +1,39 @@
 const { response } = require('express');
+const Usuario = require('../models/usuario');
 
 const crearUsuario = async (req, res = response) => {
 
-    const { nombre, email, password } = req.body;
+    try {
 
-    res.json({
-        ok: true,
-        msg: 'new',
-        email,
-        nombre,
-        password
-    });
+        const { email, password } = req.body;
+
+        // Verificar que el email no exista
+        const existeEmail = await Usuario.findOne({ email });
+        if (existeEmail) {
+            res.status(400).json({
+                ok: false,
+                msg: 'El correo ya existe'
+            });
+        }
+
+        // TODO: encriptar contraseña
+
+        // Guardar usuario en BD
+        const usuario = new Usuario(req.body);
+
+        await usuario.save();
+
+        res.json({
+            usuario
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
 }
 
 const login = async (req, res) => {
