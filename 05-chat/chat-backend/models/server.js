19074a97ac93f88200447bcc50aacd987add84f1
Maximiliano Cóppola <maxicopp@outlook.com>
2022-10-23 22:50:29 -0300

    Obtener todos los mensajes de chat

--- 05-chat/chat-backend/models/server.js
+++ 05-chat/chat-backend/models/server.js
@@ -35,8 +35,9 @@ class Server {
         // Parseo del body
         this.app.use(express.json());
 
-        // API ENDPoints
+        // API End Points
         this.app.use('/api/login', require('../router/auth'));
+        this.app.use('/api/mensajes', require('../router/mensajes'));
     }
 
     // Esta configuración se puede tener aquí o como propieda de clase
