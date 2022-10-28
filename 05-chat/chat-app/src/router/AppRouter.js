3f42490bef7178509a719bd59840d0d5fd177ffd
Maximiliano Cóppola <maxicopp@outlook.com>
2022-10-28 03:07:26 -0300

    Rutas secundarias

--- 05-chat/chat-app/src/router/AppRouter.js
+++ 05-chat/chat-app/src/router/AppRouter.js
@@ -6,17 +6,15 @@ import {
     Redirect
 } from 'react-router-dom';
 import { ChatPage } from '../pages/ChatPage';
-import { LoginPage } from '../pages/LoginPage';
-import { RegisterPage } from '../pages/RegisterPage';
+import { AuthRouter } from './AuthRouter';
 
 export const AppRouter = () => {
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route path="/login" component={LoginPage} />
-                    <Route path="/register" component={RegisterPage} />
-                    <Route path="/" component={ChatPage} />
+                    <Route path="/auth" component={AuthRouter} />
+                    <Route exact path="/" component={ChatPage} />
 
                     <Redirect to="/" />
                 </Switch>
