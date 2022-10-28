73a25f03bbaf89ba86541ef0196cbdd342ca65f7
Maximiliano Cóppola <maxicopp@outlook.com>
2022-10-28 03:33:14 -0300

    Diseño de Login y Registro

--- 05-chat/chat-app/src/router/AuthRouter.js
+++ 05-chat/chat-app/src/router/AuthRouter.js
@@ -3,13 +3,21 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 import { LoginPage } from '../pages/LoginPage';
 import { RegisterPage } from '../pages/RegisterPage';
 
+import '../css/login-register.css';
+
 export const AuthRouter = () => {
     return (
-        <Switch>
-            <Route exact path="/auth/login" component={LoginPage} />
-            <Route exact path="/auth/register" component={RegisterPage} />
+        <div className="limiter">
+            <div className="container-login100">
+                <div className="wrap-login100 p-t-50 p-b-90">
+                    <Switch>
+                        <Route exact path="/auth/login" component={LoginPage} />
+                        <Route exact path="/auth/register" component={RegisterPage} />
 
-            <Redirect to="/auth/login" />
-        </Switch>
+                        <Redirect to="/auth/login" />
+                    </Switch>
+                </div>
+            </div>
+        </div>
     )
 }
