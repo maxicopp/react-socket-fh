57f5f554254985ee80b980f383527f95c969d314
Maximiliano Cóppola <maxicopp@outlook.com>
2022-11-09 02:57:29 -0300

    Rutas privadas y públicas

--- 05-chat/chat-app/src/router/AppRouter.js
+++ 05-chat/chat-app/src/router/AppRouter.js
@@ -2,16 +2,19 @@ import React, { useContext, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
     Redirect
 } from 'react-router-dom';
 import { AuthContext } from '../auth/AuthContext';
 import { ChatPage } from '../pages/ChatPage';
 import { AuthRouter } from './AuthRouter';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
 
     const { auth, verificaToken } = useContext(AuthContext);
+
+    console.log(auth);
 
     useEffect(() => {
         verificaToken();
@@ -25,9 +28,17 @@ export const AppRouter = () => {
         <Router>
             <div>
                 <Switch>
-                    <Route path="/auth" component={AuthRouter} />
-                    <Route exact path="/" component={ChatPage} />
-
+                    <PublicRoute
+                        isAuthenticated={auth.logged}
+                        path="/auth"
+                        component={AuthRouter}
+                    />
+                    <PrivateRoute
+                        isAuthenticated={auth.logged}
+                        exact
+                        path="/"
+                        component={ChatPage}
+                    />
                     <Redirect to="/" />
                 </Switch>
             </div>
