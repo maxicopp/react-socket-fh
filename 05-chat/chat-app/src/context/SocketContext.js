b2eea37dad005892c1f374238df5a6bad599a649
Maximiliano Cóppola <maxicopp@outlook.com>
2022-11-12 22:56:19 -0300

    Acción de cargar usuarios

--- 05-chat/chat-app/src/context/SocketContext.js
+++ 05-chat/chat-app/src/context/SocketContext.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect } from 'react';
 import { createContext } from 'react';
 import { AuthContext } from '../auth/AuthContext';
 import { useSocket } from '../hooks/useSocket'
+import { types } from '../types/types';
+import { ChatContext } from './chat/ChatContext';
 
 export const SocketContext = createContext();
 
@@ -9,6 +11,7 @@ export const SocketProvider = ({ children }) => {
 
     const { socket, online, conectarSocket, desconectarSocket } = useSocket('http://localhost:8080');
     const { auth } = useContext(AuthContext);
+    const { dispatch } = useContext(ChatContext);
 
     useEffect(() => {
         if (auth.logged) {
@@ -24,9 +27,12 @@ export const SocketProvider = ({ children }) => {
 
     useEffect(() => {
         socket?.on('lista-usuarios', (usuarios) => {
-            console.log(usuarios);
+            dispatch({
+                type: types.usuariosCargados,
+                payload: usuarios
+            });
         })
-    }, [socket]);
+    }, [dispatch, socket]);
 
     return (
         <SocketContext.Provider value={{ socket, online }}>
---
+++ 05-chat/chat-app/src/types/types.js
@@ -0,0 +1,6 @@
+
+
+
+export const types = {
+    usuariosCargados: '[Chat] Usuarios cargados'
+};
